0e1c4d615f98e25390e944da67d6e02ca2ad2317
David Lechner <david@lechnology.com>
2024-04-17 12:25:50 -0400

    regmap: Allow missing device in regmap_name_read_file()
    [ Upstream commit 12ae3808c160b7be0de3c633ac4cbe8c5f2937bf ]
    
    This fixes a possible NULL pointer dereference oops in
    regmap_name_read_file() when the regmap does not have a device
    associated with it. For example syscon regmaps retrieved with
    syscon_regmap_lookup_by_compatible() don't have a device.
    
    Signed-off-by: David Lechner <david@lechnology.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Stable-dep-of: d3601857e14d ("regmap: debugfs: Fix a erroneous check after snprintf()")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Harshit Mogalapalli <harshit.m.mogalapalli@oracle.com>

--- drivers/base/regmap/regmap-debugfs.c
+++ drivers/base/regmap/regmap-debugfs.c
@@ -41,6 +41,7 @@ static ssize_t regmap_name_read_file(struct file *file,
 				     loff_t *ppos)
 {
 	struct regmap *map = file->private_data;
+	const char *name = "nodev";
 	int ret;
 	char *buf;
 
@@ -48,7 +49,10 @@ static ssize_t regmap_name_read_file(struct file *file,
 	if (!buf)
 		return -ENOMEM;
 
-	ret = snprintf(buf, PAGE_SIZE, "%s\n", map->dev->driver->name);
+	if (map->dev && map->dev->driver)
+		name = map->dev->driver->name;
+
+	ret = snprintf(buf, PAGE_SIZE, "%s\n", name);
 	if (ret < 0) {
 		kfree(buf);
 		return ret;
